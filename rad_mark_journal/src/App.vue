84d89e1e0cf8f211dd6a1c9876f00a8521784bcb
= <=>
2020-11-14 23:55:38 +0300

    ladno

---
+++ rad_mark_journal/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-app>
+    <v-main>
+
+      <mark-app-bar/>
+      <mark-content/>
+      <mark-footer/>
+     
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+
+import MarkFooter from "./components/Footer.vue"
+import MarkContent from "./components/Content.vue"
+import MarkAppBar from "./components/Header.vue"
+
+export default {
+  name: 'App',
+
+  components: {
+    MarkAppBar,
+    MarkContent,
+    MarkFooter
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
